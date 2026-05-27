2161561a93dadb8f972df3bc3859f5e47a81874a
Johannes <johannes.barjak@gmail.com>
2026-05-27 15:24:33 -0500

    Delete distrobox containers.

--- modules/containers.nix
+++ modules/containers.nix
@@ -9,16 +9,6 @@
   modules.containers.home = {...}: {
     programs.distrobox = {
       enable = true;
-      containers = {
-        common-debian = {
-          additional_packages = "curl libncurses-dev libnss3 libasound2t64 libxkbfile1 libpulse0";
-          image = "debian:13";
-          init_hooks = [
-            "ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker"
-            "ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker-compose"
-          ];
-        };
-      };
     };
   };
 }
